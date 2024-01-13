53e301bf8abeab12a05ca088ab022dbc3c1e50ff
SyazanieAmirin <syazamirin@outlook.com>
2024-01-13 16:15:30 +0800

    Finalised edit item's page's header

---
+++ HTML/Dashboard/Supplier/ProductDetails/EditItem/edit_item_header_supplier.php
@@ -0,0 +1,6 @@
+<div class = "menu-header">
+    <div class = "menu-header-title">
+        <h3 id = "menu-header-title-title">Edit Item</h3>
+    </div>
+    <a href = "product_details_supplier.php" class = "menu-header-go-back-btn"><button class = "btn-single">Go Back</button></a>
+</div>
